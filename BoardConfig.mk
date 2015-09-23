478f789a5332f39d20178a3a51589d38217aa75e
Josue Rivera <prbassplayer@gmail.com>
2015-09-24 00:43:31 +0200

    Geeb: Fix init_shell denial
      * avc:  denied  { set } for property=ctl.mpdecision scontext=u:r:init_shell:s0 tcontext=u:object_r:ctl_default_prop:s0 tclass=property_service
      * Thanks android-andi
    
    Change-Id: I2020e623e07ff22c35b4671f8f4f679a2162624c
    Signed-off-by: Josue Rivera <prbassplayer@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -121,6 +121,7 @@ BOARD_SEPOLICY_UNION += \
        file.te \
        file_contexts \
        hostapd.te \
+       init_shell.te \
        kickstart.te \
        mediaserver.te \
        mpdecision.te \
